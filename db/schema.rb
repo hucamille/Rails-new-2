0ea6130a0dd3b408a0bb89fdc7e0bd4607b4e4f1
Camille Hug <hug.camille@hotmail.fr>
2022-02-05 12:05:08 +0100

    Rails Task Manager

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,10 @@
 ActiveRecord::Schema.define(version: 2022_02_05_094626) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
